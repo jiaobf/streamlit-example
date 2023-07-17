b6765459d12758e153497adcace51038d4fe85cc
jiaobf <34901206+jiaobf@users.noreply.github.com>
2023-07-17 15:33:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,20 @@ with st.echo(code_location='below'):
         y = radius * math.sin(angle)
         data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+import datetime
+import streamlit as st
+
+
+st.subheader('西南多网格预报系统')
+
+
+col1, col2 = st.columns(2)
+with col1:
+    d = st.date_input("模式起报日期", datetime.date(2022, 10, 13))
+with col2:
+    t = st.time_input('模式起报时间', datetime.time(0, 0),step = 12*3600 )
+init = datetime.datetime.combine(d, t)
+        
+fhr = st.slider('fhr', min_value=18, max_value=132, step=6)
+image=fr".\{init:%Y%m%d%H}\wrfplt_Single_pcp6_f{fhr}_NE.png"
+st.image(image, caption='6h降水预报',use_column_width='auto')    
